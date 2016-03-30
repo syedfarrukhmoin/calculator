097ff5d1cf4f54b69b8816d20ddcea8729201632
syedfarrukhmoin <farrukh.syedmoin@gmail.com>
2016-03-30 23:16:49 +0500

    Added files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,55 @@
+import java.sql.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static spark.Spark.*;
+import spark.template.freemarker.FreeMarkerEngine;
+import spark.ModelAndView;
+import static spark.Spark.get;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+   double n1, n2;
+        String operation;
+        Scanner scannerObject = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        n1 = scannerObject. nextDouble();
+
+        System.out.println("Enter second number");
+        n2 = scannerObject. nextDouble();
+
+        Scanner op = new Scanner(System.in);
+        System.out.println("Enter your operation");
+        operation = op.next();
+
+        switch (operation)  {
+        case "+":
+            System.out.println("Your answer is " + (n1 + n2));
+            break;
+
+        case "-":
+            System.out.println("Your answer is " + (n1 - n2));
+            break;
+
+        case "/":
+            System.out.println("Your answer is " + (n1 / n2));
+            break;
+
+        case "*":
+            System.out.println("Your asnwer is " + (n1 * n2));
+            break;
+
+        default:
+            System.out.println("Je ne sais pas");
+  }
+
+}
